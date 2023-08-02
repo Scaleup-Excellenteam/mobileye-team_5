cd04b74a13b12ca0208eff7c7b915cc2e1fdb501
Anton Nahhas <antonna@edu.hac.ac.il>
2023-08-02 12:31:42 +0300

    improved the rgb detection added an HSV mask

--- part_1/part_1/part_1.py
+++ part_1/part_1/part_1.py
@@ -8,6 +8,7 @@ from scipy import signal as sg
 from scipy.ndimage import maximum_filter, label
 from PIL import Image
 import matplotlib.pyplot as plt
+import cv2
 
 # if you wanna iterate over multiple files and json, the default source folder name is this.
 DEFAULT_BASE_DIR: str = 'INSERT_YOUR_DIR_WITH_PNG_AND_JSON_HERE'
@@ -45,6 +46,7 @@ def show_image_and_gt(c_image: np.ndarray, objects: Optional[List[POLYGON_OBJECT
             # The legend provides a visual representation of the labels associated with the plotted objects.
             # It helps in distinguishing different objects in the plot based on their labels.
             plt.legend()
+
 
 
 def create_red_kernel() -> np.array:
@@ -99,9 +101,37 @@ def threshold_image(image: np.array, threshold: int) -> np.array:
     return thresholded_image
 
 
+def create_red_hsv_mask(image: np.array) -> np.array:
+    """
+    Create a mask for the red traffic light region in the image in the HSV color space.
+    :param image: The input image.
+    :return: A binary mask where the red traffic light region is set to 1 and the rest to 0.
+    """
+    # Define the lower and upper HSV color range for red traffic lights
+    lower_red_color = np.array([0, 100, 100])    # Lower boundary for red color in HSV
+    upper_red_color = np.array([10, 255, 255])   # Upper boundary for red color in HSV
+    # Create the HSV color range mask for red traffic lights
+    hsv_image = cv2.cvtColor(image, cv2.COLOR_RGB2HSV)
+    red_mask = cv2.inRange(hsv_image, lower_red_color, upper_red_color)
+    return red_mask
+
+def create_green_hsv_mask(image: np.array) -> np.array:
+    """
+    Create a mask for the green traffic light region in the image in the HSV color space.
+    :param image: The input image.
+    :return: A binary mask where the green traffic light region is set to 1 and the rest to 0.
+    """
+    # Define the lower and upper HSV color range for green traffic lights
+    lower_green_color = np.array([45, 110, 110])    # Lower boundary for green color in HSV
+    upper_green_color = np.array([150, 255, 255])    # Upper boundary for green color in HSV
+    # Create the HSV color range mask for green traffic lights
+    hsv_image = cv2.cvtColor(image, cv2.COLOR_RGB2HSV)
+    green_mask = cv2.inRange(hsv_image, lower_green_color, upper_green_color)
+    return green_mask
+
 def extract_tfl_coordinates(image: np.array, red_threshold: int, green_threshold: int) -> Tuple[RED_X_COORDINATES, RED_Y_COORDINATES, GREEN_X_COORDINATES, GREEN_Y_COORDINATES]:
     """
-    Extract the coordinates of red and green traffic lights from the image.
+    Extract the coordinates of red and green traffic lights from the image using HSV color range.
     :param image: The input image.
     :param red_threshold: The threshold value for red traffic lights.
     :param green_threshold: The threshold value for green traffic lights.
@@ -118,13 +148,22 @@ def extract_tfl_coordinates(image: np.array, red_threshold: int, green_threshold
     normalize_kernel(red_kernel)
     normalize_kernel(green_kernel)
 
-    # Get the red and green channels of the image
-    red_channel = image[:, :, 0]
-    green_channel = image[:, :, 1]
+    # Get the red and green channels of the image in the HSV color space
+    hsv_image = cv2.cvtColor(image, cv2.COLOR_RGB2HSV)
+    red_channel_hsv = hsv_image[:, :, 0]
+    green_channel_hsv = hsv_image[:, :, 0]
+
+    # Create HSV masks for red and green traffic lights
+    red_mask = create_red_hsv_mask(image)
+    green_mask = create_green_hsv_mask(image)
+
+    # Apply the HSV masks to the red and green channels
+    red_channel_filtered = red_channel_hsv * red_mask
+    green_channel_filtered = green_channel_hsv * green_mask
 
     # Perform convolution for red and green channels
-    conv_red = convolution(red_channel, red_kernel)
-    conv_green = convolution(green_channel, green_kernel)
+    conv_red = convolution(red_channel_filtered, red_kernel)
+    conv_green = convolution(green_channel_filtered, green_kernel)
 
     # Threshold the convolved images
     red_thresholded = threshold_image(conv_red, red_threshold)
@@ -139,6 +178,7 @@ def extract_tfl_coordinates(image: np.array, red_threshold: int, green_threshold
     green_coordinates = np.argwhere(green_filtered)
 
     return red_coordinates[:, 0], red_coordinates[:, 1], green_coordinates[:, 0], green_coordinates[:, 1]
+
 
 
 def test_find_tfl_lights(image_path: str, image_json_path: Optional[str]=None, fig_num=None):
@@ -158,9 +198,9 @@ def test_find_tfl_lights(image_path: str, image_json_path: Optional[str]=None, f
 
     show_image_and_gt(c_image, objects, fig_num)
 
-    # Set the threshold values for red and green traffic lights (you can play with these values)
-    red_threshold = 860
-    green_threshold = 860
+    # Set the threshold values for red and green traffic lights
+    red_threshold = 330
+    green_threshold = 150
 
     red_x, red_y, green_x, green_y = extract_tfl_coordinates(c_image, red_threshold, green_threshold)
     # 'ro': This specifies the format string. 'r' represents the color red, and 'o' represents circles as markers.
